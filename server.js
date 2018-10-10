f1295368f4657743b0f6d5284474754c9a3b5a5e
Deeter <deeter.cesler@gmail.com>
2018-10-10 10:41:37 -0600

    object names display on index.ejs

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const Data = require("./models/data");
 
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        data: Data
+    });
+})
 
 app.listen(3000, ()=>{
     console.log("server is running zoooooom");
