3a0491acb4b6c3ee41de6bc389442d5fd295433b
Deeter <deeter.cesler@gmail.com>
2018-10-10 10:50:20 -0600

    made delete functionality on the index.ejs template, and it works

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const Data = require("./models/data");
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser')
 
+// middleware
+app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// routes
 app.get("/", (req, res) => {
     res.render("index.ejs", {
         data: Data
     });
+})
+
+app.delete("/:id/delete", (req, res) => {
+    Data.splice([req.params.id],1);
+    res.redirect("/");
 })
 
 app.listen(3000, ()=>{
